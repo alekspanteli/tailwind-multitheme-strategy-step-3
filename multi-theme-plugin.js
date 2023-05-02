b6ff7d2dd515f31164da91d64f543242118a3d70
alekspanteli <alekspanteli@rambler.ru>
2023-05-02 13:06:34 +0000

    wip

--- multi-theme-plugin.js
+++ multi-theme-plugin.js
@@ -15,23 +15,40 @@ function getRgbChannels(hex) {
 
 function getCssVariableDeclarations(input, path = [], output = {}) {
   Object.entries(input).forEach(([key, value]) => {
-    const newPath = path.concat(key)
+    const newPath = path.concat(key);
     if (typeof value !== "string") {
-      getCssVariableDeclarations(value, newPath, output)
-    }
-    else{
-       output[`--${newPath.join('-')}`] = getRgbChannels(value)
+      getCssVariableDeclarations(value, newPath, output);
+    } else {
+      output[`--${newPath.join("-")}`] = getRgbChannels(value);
     }
   });
 
   return output;
 }
 
+// Generate color extension object
+function getColorUtilitiesWithCssVariableReferences(input, path = []) {
+  return Object.fromEntries(
+    Object.entries(input).map(([key, value]) => {
+      const newPath = path.concat(key);
+      if (typeof value !== "string") {
+        return [
+          key,
+          getColorUtilitiesWithCssVariableReferences(value, newPath),
+        ];
+      } else {
+        return [key, `rgb(var(--${newPath.join("-")}) / <alpha-value>)`];
+      }
+    })
+  );
+}
+
 module.exports = plugin(
   function ({ addBase }) {
+    console.log(getColorUtilitiesWithCssVariableReferences(themes.base));
     addBase({
-      ':root': getCssVariableDeclarations(Object.values(themes)[0]),
-    })
+      ":root": getCssVariableDeclarations(Object.values(themes)[0]),
+    });
 
     Object.entries(themes).forEach(([key, value]) => {
       addBase({
@@ -42,20 +59,7 @@ module.exports = plugin(
   {
     theme: {
       extend: {
-        colors: {
-          primary: {
-            50: "rgb(var(--primary-50) / <alpha-value>)",
-            100: "rgb(var(--primary-100) / <alpha-value>)",
-            200: "rgb(var(--primary-200) / <alpha-value>)",
-            300: "rgb(var(--primary-300) / <alpha-value>)",
-            400: "rgb(var(--primary-400) / <alpha-value>)",
-            500: "rgb(var(--primary-500) / <alpha-value>)",
-            600: "rgb(var(--primary-600) / <alpha-value>)",
-            700: "rgb(var(--primary-700) / <alpha-value>)",
-            800: "rgb(var(--primary-800) / <alpha-value>)",
-            900: "rgb(var(--primary-900) / <alpha-value>)",
-          },
-        },
+        colors: getColorUtilitiesWithCssVariableReferences(Object.values(themes)[0])
       },
     },
   }
